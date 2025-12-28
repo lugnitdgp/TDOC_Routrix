3fb8d91e88851aabe475b30ecb533e74a81c4a62
Prithvish <mandalprithvish@gmail.com>
2025-12-28 08:02:31 +0530

    Add circuit breaker fields to backend model

--- internal/core/server.go
+++ internal/core/server.go
@@ -1,2 +1,23 @@
 package core
 
+import (
+	"time"
+	"sync"
+)
+
+type Backend struct {
+	Address      string
+	Weight       int
+	Alive        bool
+	ActiveConns  int64
+	Latency      time.Duration
+	ErrorCount   int64
+
+	// Circuit Breaker fields
+	CircuitState string    // CLOSED, OPEN, HALF_OPEN
+	FailureCount int64
+	LastFailure  time.Time
+	LastSuccess  time.Time
+
+	Mutex sync.Mutex
+}
